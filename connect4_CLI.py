cb1ceb1d2e7cbac1d39e7a4ea2b93f8e326049d1
Spalding Tech <T.Gash@SpaldingTech.com>
2023-12-27 20:23:56 -0500

    finished connect4

--- connect4_CLI.py
+++ connect4_CLI.py
@@ -1,9 +1,12 @@
 import numpy as np
 import pygame
 import sys
+import math
 
 BLUE = (0,0,255)
 BLACK = (0,0,0)
+RED = (255,0,0)
+YELLOW = (255, 255, 0)
 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
@@ -55,7 +58,16 @@ def draw_baord(board):
     for c in range(COLUMN_COUNT):
         for r in range (ROW_COUNT):
             pygame.draw.rect(screen, BLUE, (c*SQUARE_SIZE, r*SQUARE_SIZE+SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
-            pygame.draw.circle(screen, BLACK, (c*SQUARE_SIZE+SQUARE_SIZE/2, r*SQUARE_SIZE+SQUARE_SIZE+SQUARE_SIZE/2), RADIUS)
+            pygame.draw.circle(screen, BLACK, (int(c*SQUARE_SIZE+SQUARE_SIZE/2), int(r*SQUARE_SIZE+SQUARE_SIZE+SQUARE_SIZE/2)), RADIUS)
+
+    for c in range(COLUMN_COUNT):
+        for r in range (ROW_COUNT):
+            if board[r][c] == 1:
+                pygame.draw.circle(screen, RED, (int(c*SQUARE_SIZE+SQUARE_SIZE/2), height-int(r*SQUARE_SIZE+SQUARE_SIZE/2)), RADIUS)
+            elif board[r][c] ==2:
+                pygame.draw.circle(screen, YELLOW, (int(c*SQUARE_SIZE+SQUARE_SIZE/2), height-int(r*SQUARE_SIZE+SQUARE_SIZE/2)), RADIUS)
+    pygame.display.update()
+
 
 
 board = create_board()
@@ -78,42 +90,61 @@ screen = pygame.display.set_mode(size)
 draw_baord(board)
 pygame.display.update()
 
+myfont = pygame.font.SysFont("monospace", 75)
+
 while not game_over:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        
+        if event.type == pygame.MOUSEMOTION:
+            pygame.draw.rect(screen, BLACK, (0,0, width, SQUARE_SIZE))
+            posx = event.pos[0]
+            if turn == 0:
+                pygame.draw.circle(screen, RED, (posx, int(SQUARE_SIZE/2)), RADIUS)
+            else:
+                pygame.draw.circle(screen, YELLOW, (posx, int(SQUARE_SIZE/2)), RADIUS)
+        pygame.display.update()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            print(event.pos)
-'''
+            pygame.draw.rect(screen, BLACK, (0,0, width, SQUARE_SIZE))
+            # print(event.pos)
             # Ask for Player 1 Input
             if turn == 0:
-                col = int(input("Player 1 Make your Selection (0-6): "))
+                posx = event.pos[0]
+                col = int(math.floor(posx/SQUARE_SIZE))
 
                 if is_valid_location(board, col):
                     row = get_next_open_row(board, col)
                     drop_piece(board, row, col, 1)
 
                     if winning_move(board, 1):
-                        print("PLAYER 1 Wins!!!! Congrats!!!")
+                        # print("PLAYER 1 Wins!!!! Congrats!!!")
+                        label = myfont.render("Player 1 wins!!", 1, RED)
+                        screen.blit(label, (40,10))
                         game_over = True
-
 
             # Ask for Player 2 Input
             else:
-                col = int(input("Player 2 Make your Selection (0-6): "))
+                posx = event.pos[0]
+                col = int(math.floor(posx/SQUARE_SIZE))
 
                 if is_valid_location(board, col):
                     row = get_next_open_row(board, col)
                     drop_piece(board, row, col, 2)
 
                     if winning_move(board, 2):
-                        print("PLAYER 2 Wins!!!! Congrats!!!")
+                        #print("PLAYER 2 Wins!!!! Congrats!!!")
+                        label = myfont.render("Player 2 wins!!", 1, YELLOW)
+                        screen.blit(label, (40,10))
                         game_over = True
-            
+
             print_board(board)
+            draw_baord(board)
 
             turn += 1
             turn = turn % 2
-'''
+
+            if game_over:
+                pygame.time.wait(3000)
